c8e31f485a3da1b2909cdbf41114d6862f176292
Mahmoud Safan <46950214+MahmoudSafan@users.noreply.github.com>
2019-07-14 06:33:35 +0200

    279B - Book.cpp
    using Two-Pointers

---
+++ 279B - Book.cpp
@@ -0,0 +1,33 @@
+                                      //Two-Pointers (Book)279-b div-2
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+const int e=1e5+5;
+int t,n,arr[e];
+
+int twoPointers(){
+    int left=0,right=0,bestScore=0,sum=0;
+
+    while(right<n){
+        sum+=arr[right];
+
+        if(sum<=t)
+            bestScore=max(bestScore,right-left+1);
+
+        while(sum>t)
+            sum-=arr[left++];
+
+        right++;
+
+    }
+return bestScore;
+}
+int main()
+{
+    scanf("%d%d",&n,&t);
+    for(int i=0;i<n;i++)    scanf("%d",arr+i);
+    printf("%d\n",twoPointers());
+    return 0;
+}
+
+//MG-M.safan
